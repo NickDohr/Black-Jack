53370a1ea7679075f08f1ad23be9ac170c1c826f
nicholas.dohr <nicholas.dohr@MU-C1-09.mullen.pvt.k12.co.us>
2021-10-04 11:41:54 -0600

    Finished Blackjack without aces or splitting changes

--- Black Jack Game/src/GameRunner.java
+++ Black Jack Game/src/GameRunner.java
@@ -8,12 +8,14 @@ public class GameRunner
 		static Scanner userIntInput = new Scanner(System.in);
 		static int totalDealerValue;
 		static int totalPlayerValue;
+		static boolean dealerHanding = true;
+		static boolean playerHanding = true;
 		public static void main(String[] args)
 		{
 			greetUser();
 			Deck.shuffle();
 			dealCards();
-			displayUserHand();
+		
 			playGame();
 			
 		}
@@ -43,34 +45,23 @@ public class GameRunner
 					totalDealerValue = totalDealerValue + dealerHand.get(i).getValue();
 				}
 		}
-		// combine into 1 class
-		public static void displayUserHand()
-			{
-			
-				if(totalPlayerValue >= 21)
-					{
-						checkForWinner();
-					}
-				System.out.println("User, your hand is: ");
-				for(int i = 0; i < playerHand.size(); i++)
-					{
-				System.out.println(playerHand.get(i).getRank() + " of "+ playerHand.get(i).getSuit() + " with a value of " + playerHand.get(i).getValue());
-				
-					}
-				
-				System.out.println("The value of all of your cards right now is: "+ totalPlayerValue);
-				playGame();
-			}
+
+		
 		public static void playGame()
 		{
-			System.out.println();
-			//System.out.println("The value of all of your cards right now is: "+ totalPlayerValue);
-			
-			if(totalPlayerValue == 21 && totalDealerValue == 21)
+			System.out.println("User, your hand is: ");
+			for(int i = 0; i < playerHand.size(); i++)
 				{
-					System.out.println("You and the dealer both had a total value of 21, therefore it is a push and neither of you lose money!");
+			System.out.println(playerHand.get(i).getRank() + " of "+ playerHand.get(i).getSuit() + " with a value of " + playerHand.get(i).getValue());
+			
 				}
 			
+			System.out.println();
+			System.out.println("The value of all of your cards right now is: "+ totalPlayerValue);
+			
+			
+			while(playerHanding = true)
+				{
 			System.out.println("User, do you want to hit or stand?");
 			String playDecision = userStringInput.nextLine();
 			
@@ -79,59 +70,102 @@ public class GameRunner
 					playerHand.add(Deck.deck.get(0));
 					totalPlayerValue = totalPlayerValue + Deck.deck.get(0).getValue();
 					
+					
+					System.out.println("User, you drew a ");
+					System.out.println(Deck.deck.get(0).getRank() + " of "+ Deck.deck.get(0).getSuit() + " with a value of " + Deck.deck.get(0).getValue());
+					System.out.println("The value of all of your cards right now is: "+ totalPlayerValue);
 					Deck.deck.remove(0);
-					displayUserHand();
+					if(totalPlayerValue >= 21)
+						{
+							
+							checkForWinner();
+						}
 				}
 			else if(playDecision.equals("stand") || playDecision.equals("Stand"))
 				{
-					
+					playerHanding = false;
 					displayDealerHand();
-					
 				}
 			else
 				{
 					System.out.println("You must either pick hit or stand,");
 					//playGame();
 				}
+				}
 			
 		}
 		public static void displayDealerHand()
 		{
-			//if(totalDealerValue >= 17)
-				//{
+			while(dealerHanding)
+				{
+					System.out.println();
 			System.out.println("The Dealers Hand Was: ");
 			for(int i = 0; i < dealerHand.size(); i ++)
 				{
 			System.out.println(dealerHand.get(i).getRank() + " of "+ dealerHand.get(i).getSuit() + " with a value of " + dealerHand.get(i).getValue());
 				}
-			checkForWinner();
-				//}
-			//else if(totalDealerValue <=16)
+			System.out.println("The total Dealer value is " + totalDealerValue);
+			if(totalDealerValue >= 17)
+				{
+					dealerHanding = false;
+					checkForWinner();
+				}
+			else if(totalDealerValue <=16)
+				{
 					dealerHand.add(Deck.deck.get(0));
+					System.out.println();
+					System.out.print("The Dealer Drew A ");
+					System.out.print(Deck.deck.get(0).getRank() + " of "+ Deck.deck.get(0).getSuit() + " with a value of " + Deck.deck.get(0).getValue());
 					totalDealerValue = totalDealerValue + Deck.deck.get(0).getValue();
 					Deck.deck.remove(0);
-	
-			//	}
-
-			//System.out.println("The Total Dealer Value Was " + totalDealerValue);
-			checkForWinner();
+				}
+				}
 		}
 		public static void checkForWinner()
 		{
-			if(totalPlayerValue == 21 && totalDealerValue == 21)
+			if(totalPlayerValue == totalDealerValue)
 				{
-					System.out.println("You and the dealer both had a total value of 21, therefore it is a push and neither of you lose money!");
+					System.out.println();
+					System.out.println("You and the dealer both had the same value, therefore it is a push and neither of you lose money!");
+					System.exit(0);
+				}
+			else if(totalDealerValue > 21 && totalPlayerValue <= 21)
+				{
+					System.out.println();
+					System.out.println("The Dealer went over 21, and therefore the player wins!");
+					System.exit(0);
+				}
+			else if(totalDealerValue <= 21 && totalPlayerValue > 21)
+				{
+					System.out.println();
+					System.out.println("The Dealer went over 21, and therefore the player wins!");
+					System.exit(0);
 				}
 			else if(totalPlayerValue == 21 && totalDealerValue != 21)
 				{
+					System.out.println();
 					System.out.println("The Player Won, Congratulations!");
 					System.exit(0);
 				}
 			else if(totalDealerValue == 21 && totalPlayerValue != 21)
 				{
+					System.out.println();
 					System.out.println("The Dealer has won! Too Bad!");
 					System.exit(0);
 				}
+			else if(totalDealerValue > totalPlayerValue)
+				{
+					System.out.println();
+					System.out.println("The Dealer wins! You lose!");
+					System.exit(0);
+				}
+			else if(totalDealerValue < totalPlayerValue)
+				{
+					System.out.println();
+					System.out.println("The Player Wins! The Dealer Loses!");
+					System.exit(0);
+				}
+			
 			
 			
 		}
